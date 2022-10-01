b9e47ca91f5cd87d5f2074b3609f0f94b3de02ac
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2022-10-01 22:17:12 +0800

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {AuthModule} from "./auth/auth.module";
+import {CoreModule} from "./core/core.module";
+import {TodosModule} from "./todos/todos.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule, AuthModule, CoreModule, TodosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
