14d896661b6a36426ce4a6d59dee7c94d2bce48f
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2022-10-05 20:27:42 +0800

    пуеÐgetting tasks

--- src/app/todos/components/todos/todo/tasks/tasks.component.ts
+++ src/app/todos/components/todos/todo/tasks/tasks.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
+import { TasksService } from '../../../../services/tasks.service'
+import { Observable } from 'rxjs'
+import { Task } from '../../../../models/tasks.models'
 
 @Component({
   selector: 'tl-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
+  @Input() todoId!: string
 
-  constructor() { }
+  tasks$?: Observable<Task[]>
+
+  constructor(private tasksService: TasksService) {}
 
   ngOnInit(): void {
+    this.tasks$ = this.tasksService.getTasks(this.todoId)
   }
-
 }
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -5,10 +5,11 @@ import { TodosRoutingModule } from './todos-routing.module'
 import { TodosComponent } from './components/todos/todos.component'
 import { TodoComponent } from './components/todos/todo/todo.component'
 import { FormsModule } from '@angular/forms';
-import { TasksComponent } from './components/todos/todo/tasks/tasks.component'
+import { TasksComponent } from './components/todos/todo/tasks/tasks.component';
+import { TaskComponent } from './components/todos/todo/tasks/task/task.component'
 
 @NgModule({
-  declarations: [TodosComponent, TodoComponent, TasksComponent],
+  declarations: [TodosComponent, TodoComponent, TasksComponent, TaskComponent],
   imports: [CommonModule, TodosRoutingModule, FormsModule],
 })
 export class TodosModule {}
