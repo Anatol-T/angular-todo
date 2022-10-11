fa7c795d4759ad798c5b006e05be2ac34c86337b
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2022-10-11 11:38:19 +0800

    added some stile

---
+++ src/app/todos/components/todos/todo/todo-filters/todo-filters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tl-todo-filters',
+  templateUrl: './todo-filters.component.html',
+  styleUrls: ['./todo-filters.component.css']
+})
+export class TodoFiltersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
