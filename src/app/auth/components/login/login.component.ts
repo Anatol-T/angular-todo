c79c3ddb040afdfaf95e4cee6706f5c694fa99ad
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2022-10-15 10:38:07 +0800

    login component

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'tl-login',
@@ -6,7 +7,32 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    rememberMe: new FormControl(false),
+  })
+  mail = ''
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  get email() {
+    return this.loginForm.get('email')
+  }
+  get password() {
+    return this.loginForm.get('password')
+  }
+
+  onSubmit() {
+    const value = this.loginForm.value
+    this.mail = JSON.stringify(this.email?.value)
+  }
 }
